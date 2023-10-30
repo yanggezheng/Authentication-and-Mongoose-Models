b89b7e4eb48b3f879600a35c321478ee0b2767c0
Yangge Zheng <yanggezheng@yanggesmbp.mynetworksettings.com>
2023-10-29 21:33:45 -0400

    done

--- src/app.mjs
+++ src/app.mjs
@@ -2,6 +2,8 @@ import './db.mjs';
 import express from 'express';
 import mongoose from 'mongoose';
 import session from 'express-session';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
 
 import path from 'path';
 import url from 'url';
@@ -10,9 +12,9 @@ import * as auth from './auth.mjs';
 const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
 
 const app = express();
-
+const server = createServer(app);
+const io = new Server(server);
 app.set('view engine', 'hbs');
-
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: false }));
 app.use(session({
@@ -21,20 +23,17 @@ app.use(session({
     saveUninitialized: true,
 }));
 
-const Article = mongoose.model('Article');
+const Task = mongoose.model('Task');
+const Friend = mongoose.model('Friend');
 
 const loginMessages = {"PASSWORDS DO NOT MATCH": 'Incorrect password', "USER NOT FOUND": 'User doesn\'t exist'};
 const registrationMessages = {"USERNAME ALREADY EXISTS": "Username already exists", "USERNAME PASSWORD TOO SHORT": "Username or password is too short"};
 
-
-///////////////////////
-// CUSTOM MIDDLEWARE //
-///////////////////////
-
 // require authenticated user for /article/add path
-app.use(auth.authRequired(['/article/add']));
+app.use(auth.authRequired(['/add']));
+app.use(auth.authRequired(['/addFriends']));
+app.use(auth.authRequired(['/friendsDetails']));
 
-// make {{user}} variable available for all paths
 app.use((req, res, next) => {
   res.locals.user = req.session.user;
   next();
@@ -45,40 +44,52 @@ app.use((req, res, next) => {
   console.log(req.method, req.path, req.body);
   next();
 });
-
-////////////////////
-// ROUTE HANDLERS //
-////////////////////
 app.get('/', (req, res) => {
-  Article.find({}).sort('-createdAt').exec((err, articles) => {
-    res.render('index', {user: req.session.user, home: true, articles: articles});
+  Task.find({}).sort('-createdAt').exec((err, tasks) => {
+    res.render('index', {user: req.session.user, tasks: tasks});
   });
 });
 
-app.get('/article/add', (req, res) => {
-  if (!req.session.user){
-    res.redirect("/login");
-  }else{
-    res.render('article-add');
-  }
 
+app.get('/friendsDetails', (req, res) => {
+  Friend.find({}).sort('-createdAt').exec((err, friends) => {
+    res.render('friendsDetails', {user: req.session.user, friends: friends});
+  });
 });
 
-app.post('/article/add', (req, res) => {
-  //complete POST /article/add
+app.get('/add', (req, res) => {
   if (!req.session.user){
     res.redirect("/login");
   }else{
-    const newArticle = new Article({
+    res.render('add');
+  }
+});
+
+app.get('/addFriends', (req, res) => {
+  if (!req.session.user){
+    res.redirect("/login");
+  }else{
+    res.render('addFriends');
+  }
+});
+
+
+
+app.post('/add', (req, res) => {
+  if (!req.session.user){
+    res.redirect("/login");
+  }else{
+    const newTask = new Task({
       title: req.body.title,
-      url: req.body.url,
+      dueDate: req.body.dueDate,
       description: req.body.description,
+      done: false,
       user: req.session.user._id
     });
-    newArticle.save(err => {
+    newTask.save(err => {
       if (err){
         console.log(err);
-        res.render('article-add', {error: err});
+        res.render('add', {error: err});
       }else{
         res.redirect("/");
       }
@@ -86,15 +97,48 @@ app.post('/article/add', (req, res) => {
   }
 });
 
-app.get('/article/:slug', (req, res) => {
+app.post('/addFriends', (req, res) => {
+  if (!req.session.user){
+    res.redirect("/login");
+  }else{
+    const newFriend = new Friend({
+      username: req.body.username,
+      email: req.body.email,
+      phone: req.body.phone,
+      user: req.session.user._id
+    });
+    newFriend.save(err => {
+      if (err){
+        console.log(err);
+        res.render('addFriends', {error: err});
+      }else{
+        res.redirect("/");
+      }
+    });
+  }
+});
+
+app.get('/tasks/:slug', (req, res) => {
   //complete GET /article/slug
-  Article.findOne({slug: req.params.slug})
+  Task.findOne({slug: req.params.slug})
   .populate("user")
   .exec((err, result) => {
     if (err){
       res.render('error', {message: "error"});
     }else{
-      res.render('article-detail', {article: result});
+      res.render('detail', {task: result});
+    }
+  });
+});
+
+app.get('/friends/:slug', (req, res) => {
+  Friend.findOne({slug: req.params.slug})
+  .populate("user")
+  .exec((err, result) => {
+    if (err){
+      res.render('error', {message: "error"});
+    }else{
+      res.render('friendsDetails', {friends: result});
     }
   });
 });
@@ -104,7 +148,6 @@ app.get('/register', (req, res) => {
 });
 
 app.post('/register', (req, res) => {
-  // setup callbacks for register success and error
   function success(newUser) {
     auth.startAuthenticatedSession(req, newUser, (err) => {
         if (!err) {
@@ -118,8 +161,6 @@ app.post('/register', (req, res) => {
   function error(err) {
     res.render('register', {message: registrationMessages[err.message] ?? 'Registration error'}); 
   }
-
-  // attempt to register new user
   auth.register(req.body.username, req.body.email, req.body.password, error, success);
 });
         
@@ -129,7 +170,6 @@ app.get('/login', (req, res) => {
 });
 
 app.post('/login', (req, res) => {
-  // setup callbacks for login success and error
   function success(user) {
     auth.startAuthenticatedSession(req, user, (err) => {
       if(!err) {
@@ -143,9 +183,40 @@ app.post('/login', (req, res) => {
   function error(err) {
     res.render('login', {message: loginMessages[err.message] || 'Login unsuccessful'}); 
   }
-
-  // attempt to login
   auth.login(req.body.username, req.body.password, error, success);
 });
 
-app.listen(3000);
+server.listen(3000);
+
+let left1 = 0,
+    left2 = 0;
+const off = 10;
+io.on('connection', (socket) => {
+
+    if (left1 >= 500 ) {
+        left1 = 0;
+    }
+
+    socket.emit('update', { left1, left2 });
+    
+
+    socket.on('move1', () => {
+        console.log('move1');
+        left1 += off;
+        io.emit('update', { left1, left2 });
+        
+        if (left1 >= 500) {
+            io.emit('over');
+        }
+    });
+
+    socket.on('move2', () => {
+      console.log('move2');
+      left1 -= off;
+      io.emit('update', { left1, left2 });
+      
+      if (left1 >= 500) {
+          io.emit('over');
+      }
+  });
+});
--- src/db.mjs
+++ src/db.mjs
@@ -6,17 +6,26 @@ const UserSchema = new mongoose.Schema({
     email: {type: String, required: true},
     password: {type: String, unique: true, required: true}
   });
+  const FriendSchema = new mongoose.Schema({
+    username: {type: String, required: true},
+    email: {type: String, required: true},
+    phone: {type: String, required: true},
+    user: {type: mongoose.Schema.Types.ObjectId, ref: 'User'}
+  });
 
-  const ArticleSchema = new mongoose.Schema({
+  const TaskSchema = new mongoose.Schema({
     title: {type: String, required: true},
-    url: {type: String, required: true},
+    dueDate: {type: String, required: true},
     description: {type: String, required: true},
+    done: {type: Boolean, required : true},
     user: {type: mongoose.Schema.Types.ObjectId, ref: 'User'}
   });
 
 // configure plugin
-ArticleSchema.plugin(mongooseSlugPlugin, {tmpl: '<%=title%>'});
+TaskSchema.plugin(mongooseSlugPlugin, {tmpl: '<%=title%>'});
+
 // register models
 mongoose.model('User', UserSchema);
-mongoose.model('Article', ArticleSchema);
+mongoose.model('Task', TaskSchema);
+mongoose.model('Friend', FriendSchema);
 mongoose.connect('mongodb://localhost/hw05');
